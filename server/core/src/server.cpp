908518ece937dc6b74338a7f63ddb0ad014b1d94
Nathan Dornbrook <Tr3kkR@users.noreply.github.com>
2026-03-04 22:24:19 +0000

    Restore anonymous namespace for internal server helpers

--- server/core/src/server.cpp
+++ server/core/src/server.cpp
@@ -181,7 +181,8 @@ class ServerImpl final : public Server {
 public:
     explicit ServerImpl(Config cfg)
         : cfg_(std::move(cfg)),
-          chargen_state_(std::make_shared<ChargenState>())
+          chargen_state_(std::make_shared<ChargenState>()),
+          agent_service_(chargen_state_)
     {
         setup_chargen_logger();
     }
@@ -201,9 +202,9 @@ private:
     void stop_chargen();
 
     Config                            cfg_;
-    std::shared_ptr<ChargenState>     chargen_state_;
-    AgentServiceImpl                  agent_service_{chargen_state_};
-    ManagementServiceImpl             mgmt_service_;
+    std::shared_ptr<ChargenState> chargen_state_;
+    AgentServiceImpl             agent_service_;
+    ManagementServiceImpl        mgmt_service_;
     std::unique_ptr<grpc::Server>     agent_server_;
     std::unique_ptr<grpc::Server>     mgmt_server_;
     std::unique_ptr<httplib::Server>  web_server_;
